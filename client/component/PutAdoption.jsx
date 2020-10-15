e8a99877ca8858ea27aa8f6489172fae05537b9e
ahmedkortas <kortas.ahmed.dodo@gmail.com>
2020-10-15 15:29:22 +0200

    greated the view switcher

---
+++ client/component/PutAdoption.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class PutAdoption extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>This is the PutAdoption</h1>;
+  }
+}
+
+export default PutAdoption;
