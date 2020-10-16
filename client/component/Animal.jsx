86b86faef2053813c79ebbc0d6d0cbf8948329a9
benthabet.chayma@gmail.com <benthabet.chayma@gmail.com>
2020-10-16 10:05:26 +0100

    bootstrap is working

--- client/component/Animal.jsx
+++ client/component/Animal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card ,Button} from "react-bootstrap";
 
 class Animal extends React.Component {
   constructor(props) {
@@ -6,13 +7,26 @@ class Animal extends React.Component {
   }
   render() {
     let data = this.props.data;
-    return (
-      <div className="card">
-        {console.log(this.props)}
-        <span>my name:{data.name}</span>
-        <img src={data.imageUrl} style={{ width: "50%" }}></img>
-      </div>
-    );
+   // return (
+  //    <div className="card">
+  //      {console.log(this.props)}
+   //     <span>my name:{data.name}</span>
+   //     <img src={data.imageUrl} style={{ width: "50%" }}></img>
+   //   </div>
+   // );
+   
+      return(<div>
+      <Card className="mb-3" style={{ color :"#000"}}>
+      <Card.Img src={data.imageUrl} style={{ width: "600px", padding: "2px" ,height:"300px"}}/> 
+      <Card.Body> 
+      <Card.Title> my name:{data.name} </Card.Title>
+      <Card.Text> {data.description} </Card.Text>
+     
+      <Button variant="success"> Adopt </Button>
+      </Card.Body>
+      </Card>
+      </div>)
+    
   }
 }
 
